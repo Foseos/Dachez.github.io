39ca8b9d642330ca521fbadbfa933113a65e95d2
Foseos <lucasdachez@yahoo.fr>
2022-10-03 21:27:48 +0200

    En attentes des modification
    A REVOIR
    * % compétences ?
    * Compétences : éléments de l'année dernière + actuelle (cf. CV SLAM et/ou plaquette SLAM)
    * Tableau de compétences en PDF
    * Compétences : ajouter lien vers plaquette SLAM
    * CV à compléter (+technique) - cf. CV SLAM
    * AP promo précédente : un descriptif + quelques éléments techniques
    
    A FAIRE
    Épreuves

---
+++ .history/src/composants/NavBar_20220917150040.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import prenom from '../assets/img/prenom.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import CV from '../assets/img/CV_LUCAS.pdf';
+
+
+export const NavBar = () => {
+
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  return (
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={prenom} alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className={activeLink === 'Accueil' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Accueil</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'Compétences' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Compétences</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'Projets' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projets</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/lucas-dachez-467244243"><img src={navIcon1} alt="" /></a>
+              <a href="https://github.com/Foseos?tab=repositories"><img src={navIcon2} alt="" /></a>
+              
+            </div>
+            <a href="/Dachez.githubCV_LUCAS.pdf" download='CV_LUCAS.pdf'>
+                <button className="vvd"><span>Télécharger mon CV</span></button>
+              </a>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
---
+++ .history/src/composants/Projects_20220917153144.js
@@ -0,0 +1,147 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard2, ProjectCard3, ProjectCard, ProjectCard4 } from "./ProjectCard";
+import projImg4 from "../assets/img/project-img4.jpg";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import porfolio from "../assets/img/PortFolio_1.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      id : 2,
+      title: "AMAP",
+      description: "Modification du site d'AMAP pour répondre a un besoin",
+      imgUrl: projImg2,
+    },
+    {
+      id: 2,
+      title: "Axéréal",
+      description: "Mise en place d'un script bash et d'une interface web pour la gestion des sauvegardes",
+      imgUrl: projImg3,
+    },
+    {
+      id: 3,
+      title: "Pecheur.com",
+      description: "Création d'une interface web pour trier est archiver les erreurs du programme web",
+      imgUrl: projImg4,
+    },
+    {
+      id: 2,
+      title: "PortFolio",
+      description: "Création d'un PortFolio en HTML/CSS",
+      imgUrl: porfolio,
+    },
+    {
+      id: 3,
+      title: "SNCF",
+      description: " Mise à jour de l'interface graphique ",
+      imgUrl: porfolio,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projets</h2>
+                <p>Voici tous les projets que j'ai pu réaliser que ce soit en stage mais aussi bien durant ma formation.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">ALL</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Web site</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Application</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="four">Réseau</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>                  
+                    <Tab.Pane eventKey="second">
+                      <p>J'ai pu travailler sur la création/amélioration/optimisation de site via principalement le langage PHP avec son framework Symfony</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard2
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p></p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard3
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="four">
+                      <p></p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="four">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard4
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
